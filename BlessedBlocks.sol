f6d536e500b2f09886529e8cd87deee69070efdd
tinfoilsombrero <tinfoilsombrero@users.noreply.github.com>
2017-10-23 12:34:25 -0400

    updated contract to actually work and improved documentation

--- BlessedBlocks.sol
+++ BlessedBlocks.sol
@@ -3,57 +3,59 @@ pragma solidity ^0.4.11;
 contract BlessedBlocks {
     mapping (address => bet) bets; //map an address to a bet
 	uint public startTime;
-	uint public endTime;
+	uint public betEndTime;
 	uint[] secrets; //storing all collected secrets
 	uint public betAmount; //the required bet amount, in finney
 	uint public numBettors;
 	uint winSecret;
 	bool winSet;
-	uint public curTime;
-	bytes32 myhash;
+//	bytes32 myhash;
 	uint finalBalance;
 	uint public revealTimeOffset;
 	uint numWinners;
-	address public temple; //the address of the initializing entity
+	address public owner; //the address of the initializing entity, currently unused
+	
 	struct bet{
 	    bool paid; //if this bet was paid out or not
 	    bytes32 hash; //the hash of the bettor address and their secret number
 	    uint secret; //the revealed secret populated during the reveal phase
         bool gotSecret; //tracks the submission of the secret	    
 	}
-	//store collected ether
-	
-	//Create a new bettor entry, passing in the hash that will be proof of win
+
+	/*Create a new bettor entry, passing in the hash that will be proof of win*/
 	function submitBet(bytes32 _hash) public payable {
 		require(msg.value == (betAmount * 1 finney));
-		require(now <= endTime);
-		require(bets[msg.sender].hash == 0);
+		require(now <= betEndTime);
+		require(bets[msg.sender].hash == 0x0);
 		bets[msg.sender].paid = false;
         bets[msg.sender].hash = _hash;
 	}
 	
+	/*bettors reveal their secret numbers, which are checked for validity against submitted hash*/
 	function revealSecret(uint _secret) public{
 	     //ensure that the checking entity has actually made a bet and that they are not resubmitting a secret
-//	    require(bets[msg.sender].hash != 0 && bets[msg.sender].gotSecret == false);
+	    require(bets[msg.sender].hash != 0 && bets[msg.sender].gotSecret == false);
 	    //check to make sure the time is after betting phase is over
-//		require(now < revealTimeOffset);
-		myhash = 0x815fb7b57f02ad10b400541d6f547e2cf0090e3e0a42b19626b756a0e39bcaec;
+		require(now < revealTimeOffset);
+//		myhash=keccak256(_secret,msg.sender);
 		//match the previously submitted hash to ensure that the bettor is honest
-//    	require(bets[msg.sender].hash == keccak256(_secret,msg.sender));
-//		bets[msg.sender].secret = _secret;
-//		bets[msg.sender].gotSecret = true;
+    	require(bets[msg.sender].hash == keccak256(_secret,msg.sender));
+    	//store secret and flag the user as submitted 
+		bets[msg.sender].secret = _secret;
+		bets[msg.sender].gotSecret = true;
+		secrets.push(_secret);
 		//keep count of how many bets have been made, do it at this stage because if you don't submit an N value you lose your deposit
 		numBettors++; 
 	}
-	function getTime() public {
-	    curTime = now;
-	}
+	
+	/*calculate the winning numbers and pay the winners*/
 	function checkWinnings() public{
 	    uint picker = 0;
 	    //time must be after the reveal window
 	    require(now > revealTimeOffset);
 	    //the checking party must have revealed their secret number and not collected their winnings yet
 	    require(bets[msg.sender].gotSecret == true && bets[msg.sender].paid == false);
+	    //check if the winner has been chosen yet, if not then kick off the process
 	    if (winSet== false) {
 	        for (uint i = 0; i < secrets.length; i++){
 	            picker = picker ^ secrets[i];
@@ -65,22 +67,22 @@ contract BlessedBlocks {
     	       if (secrets[x] == winSecret) {numWinners++;}
     	   }
     	   finalBalance = this.balance;
-    	   //set the flag
+    	   //set the flag to indicate that the winning number has been calculated
     	   winSet = true;
-	   }
+	    }
 	   if (winSecret == bets[msg.sender].secret){
-	       //send the winnings, divided by number of winners
-	       msg.sender.transfer(finalBalance/numWinners);
+	       //mark the competitor as paid, send the winnings divided by number of winners
 	       bets[msg.sender].paid = true;
+	       msg.sender.transfer(finalBalance/numWinners);
 	   }
-	}	
-
-	//function: set up the rules for the bet
+	}
+	
+	/*function: set up the rules for the bet*/
 	function BlessedBlocks(uint _timeLimit,uint _betAmount,uint _revealTimeOffset) public {
-		temple = msg.sender;
+		owner = msg.sender;
         startTime = now;
-        endTime = (_timeLimit * 1 seconds) + startTime;
-        revealTimeOffset = (_revealTimeOffset * 1 seconds) + endTime;
+        betEndTime = (_timeLimit * 1 seconds) + startTime;
+        revealTimeOffset = (_revealTimeOffset * 1 seconds) + betEndTime;
 		betAmount = _betAmount;
 	}
 }
